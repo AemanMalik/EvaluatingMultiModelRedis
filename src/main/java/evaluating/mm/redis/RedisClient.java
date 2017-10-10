185210d6514bbda41999f543f9e9a2583ff4ce40
AemanMalik <aemannawaz@live.com>
2017-10-10 18:20:52 +0200

    Successfully converted from Redis to the modules!

--- src/main/java/evaluating/mm/redis/RedisClient.java
+++ src/main/java/evaluating/mm/redis/RedisClient.java
@@ -1,14 +1,18 @@
 package evaluating.mm.redis;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Protocol;
 import com.redislabs.modules.rejson.JReJSON;
+
+import evaluating.mm.redis.utils.Commands;
 
 public class RedisClient {
 	
@@ -24,28 +28,106 @@ public class RedisClient {
 	
 	public static void read () {
 		Set<String> keys = jedis.keys("*");
-		keys.clear();
-		keys.add("1");
-		keys.add("2");
 		Iterator<String> it = keys.iterator();
 		while (it.hasNext()) {
 			String key= it.next();
-			Map<String, String> values = jedis.hgetAll(key);
-			redis.put(key, values);
+			if (key.startsWith("v")||key.startsWith("e")){
+				Map<String, String> values = jedis.hgetAll(key);
+				redis.put(key, values);
+			}
 		}
 		
 	}
 	
-	public static void convertToGraph() {
-		
+	//This converts all keys starting with v to vertexes and all starting with e to edges, in a graph with the name is passed as a parameter.
+	//So far this works for a single type of node, passed as parameter
+	//It assumes that the edge has a label (which defines the relationship type) and a src and tgt that have the nodeType values.
+	public static void convertToGraph(String graphName, String nodeType) {
+		String script = "return {redis.call('GRAPH.QUERY', KEYS[1], ARGV[1])}";
+		List<String> keys = new ArrayList<String>();
+	    keys.add(graphName);
+		redis.entrySet().stream().filter(e->e.getKey().startsWith("v")).forEach(v->{
+			List<String> args = new ArrayList<String>();
+			String query= "'CREATE (";
+			query+=v.getValue().get(nodeType)+":"+nodeType+" ";
+			query+="{ ";
+			query+=v.getValue().entrySet().stream().filter(entry->entry.getKey()!=nodeType).map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			query+="})";
+			args.add(query);
+			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
+		    /*response.forEach(resp->{
+		    	System.out.println(resp.toString());
+		    });*/
+			
+		});
+		redis.entrySet().stream().filter(e->e.getKey().startsWith("e")).forEach(v->{
+			List<String> args = new ArrayList<String>();
+			String query= "'CREATE (";
+			query+=v.getValue().get("src");
+			query+=")-["+v.getValue().get("label")+" {";
+			query+=v.getValue().entrySet().stream().filter(entry->(entry.getKey()!="label")&&entry.getKey()!="src" && entry.getKey()!="tgt").map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			query+="}]->";
+			query+="("+v.getValue().get("tgt");
+			query+=")";
+			args.add(query);
+			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
+		    /*response.forEach(resp->{
+		    	System.out.println(resp.toString());
+		    });*/
+		});
 	}
 	
+	
+	//This converts all keys starting with v to vertexes and all starting with e to edges, in a graph with the name is passed as a parameter.
+	//So far this works for a single type of node, passed as parameter
+	//It assumes that the edge has a label (which defines the relationship type) and a src and tgt that have the nodeType values.
+	public static void convertToGraphStoredProcedure(String graphName, String nodeType) {
+		String script = "return {redis.call('GRAPH.QUERY', KEYS[1], ARGV[1])}";
+		List<String> keys = new ArrayList<String>();
+	    keys.add(graphName);
+		redis.entrySet().stream().filter(e->e.getKey().startsWith("v")).forEach(v->{
+			List<String> args = new ArrayList<String>();
+			String query= "'CREATE (";
+			query+=v.getValue().get(nodeType)+":"+nodeType+" ";
+			query+="{ ";
+			query+=v.getValue().entrySet().stream().filter(entry->entry.getKey()!=nodeType).map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			query+="})";
+			args.add(query);
+			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
+		    /*response.forEach(resp->{
+		    	System.out.println(resp.toString());
+		    });*/
+			
+		});
+		redis.entrySet().stream().filter(e->e.getKey().startsWith("e")).forEach(v->{
+			List<String> args = new ArrayList<String>();
+			String query= "'CREATE (";
+			query+=v.getValue().get("src");
+			query+=")-["+v.getValue().get("label")+" {";
+			query+=v.getValue().entrySet().stream().filter(entry->(entry.getKey()!="label")&&entry.getKey()!="src" && entry.getKey()!="tgt").map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			query+="}]->";
+			query+="("+v.getValue().get("tgt");
+			query+=")";
+			args.add(query);
+			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
+		    /*response.forEach(resp->{
+		    	System.out.println(resp.toString());
+		    });*/
+		});
+	}
 	public static void convertToDocument() {
 		redis.entrySet().forEach(entry->{
-			JReJSON.set(jedis, entry.getKey().toString()+"3", entry.getValue());
+			JReJSON.set(jedis, entry.getKey().toString(), entry.getValue());
 		});
-		
 	}
+	
+	public static void convertToDocumentStoredProcedure() {
+		redis.entrySet().forEach(entry->{
+			JReJSON.set(jedis, entry.getKey().toString(), entry.getValue());
+		});
+	}
+	
+	
 	
 	public static void cleanup() {
 	    jedis.disconnect();
@@ -56,7 +138,8 @@ public class RedisClient {
 		init();
 		startTime=System.nanoTime();
 		read();
-		convertToDocument();
+		//convertToDocument();
+		convertToGraph("pokec", "profile");
 		endTime=System.nanoTime()-startTime;
 		System.out.println("Total duration = "+endTime);
 		cleanup();
