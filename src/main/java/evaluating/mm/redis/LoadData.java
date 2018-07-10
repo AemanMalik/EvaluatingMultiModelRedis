b96bafcb5ad84baf9d2460e7e22cb85d4b1475e7
Gabriel Campero Durand <campero@ovgu.de>
2018-07-10 16:18:57 +0200

    Added the mappings

---
+++ src/main/java/evaluating/mm/redis/LoadData.java
@@ -0,0 +1,166 @@
+package evaluating.mm.redis;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.redislabs.modules.rejson.Path;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Protocol;
+
+public class LoadData {
+	
+private static Jedis jedis;
+	
+	private static Map<String, String> redis;
+	public static void init () {
+		redis = new HashMap<>();
+		jedis = new Jedis("127.0.0.1", Protocol.DEFAULT_PORT, 500);
+		jedis.connect();
+	}
+
+	public static void main(String[] args) throws IOException, java.text.ParseException {
+
+		/*Map<String, ArrayList<String>> nodes = new HashMap<String, ArrayList<String>>();
+		
+		BufferedReader in = new BufferedReader(new FileReader("Data1.txt"));
+		
+		ArrayList<String> node_values = new ArrayList<String>();
+		
+		String line = "";
+        while ((line = in.readLine()) != null) {
+            String parts[] = line.split("\t");
+            node_values.add(line);*/
+        init();
+		try(Stream<java.nio.file.Path> paths = Files.walk(Paths.get("/home/campero/Desktop/current/pokec")).sorted()) {
+			List<java.nio.file.Path> pathsFound = paths.collect(Collectors.toList()); 
+			for (java.nio.file.Path path: pathsFound){
+				String pathAsString = path.toString();
+            
+				if (pathAsString.endsWith("profiles.txt")){
+				
+					BufferedReader in = new BufferedReader(new FileReader(path.toFile()));
+					String line, desc, nodeId;
+					Integer count=0; 
+					while((line = in.readLine()) != null)
+					{	
+						count++;
+						if (count< 0){
+							//do nothing
+						}
+						else{
+								String[] split = line.split("\t", -1);
+								if (split.length>9){
+									if (!redis.isEmpty()){
+										redis.clear();
+									}
+									nodeId= split[0];
+									redis.put("public", Boolean.TRUE.toString());
+									if (split[2]!=null && !split[2].contains("null")){
+										Integer completion= Integer.parseInt(split[2]);
+										redis.put("completion", completion.toString());
+									}
+									else{
+										redis.put("completion", "0");
+									}
+									if (split[3]!=null && !split[3].contains("null")){
+										Boolean gender= split[3].contains("1");
+										redis.put("gender", gender.toString());
+									}
+									else{
+										redis.put("gender", Boolean.FALSE.toString());
+									}
+									redis.put("region", split[4]);
+									redis.put("lastLogin", split[5]);
+									redis.put("registration", split[6]);
+									if (split[7]!=null && !split[7].contains("null")){
+										Integer age=Integer.parseInt(split[7]);
+										redis.put("age", age.toString());
+									}
+									else{
+										redis.put("age", "0");
+									}
+									desc="";
+									for (int i=8; i<split.length; i++){
+										desc+=split[i]+" ";
+									}
+									redis.put("desc", desc);
+									jedis.hmset("v"+nodeId, redis);
+						
+					}
+            
+        }
+						}
+					System.out.println("W00T, we loaded the vertexes");
+		}
+		else if (pathAsString.endsWith("relationships.txt")){
+						
+						BufferedReader in = new BufferedReader(new FileReader(path.toFile()));
+						String line, desc, nodeId;
+						Integer count=0; 
+						while((line = in.readLine()) != null)
+						{	
+							count++;
+							if (count< 0){
+								//do nothing
+							}
+							else{
+									String[] split = line.split("\t", -1);
+									if (split.length>1){
+										if (!redis.isEmpty()){
+											redis.clear();
+										}
+										nodeId= count.toString();
+										if (split[0]!=null && !split[0].contains("null")){
+											redis.put("src", "v"+split[0]);
+										}
+										if (split[1]!=null && !split[1].contains("null")){
+											redis.put("tgt", "v"+split[1]);
+										}
+										redis.put("label", "friendsOf");
+										if (redis.containsKey("tgt")&&redis.containsKey("src")){
+											jedis.hmset("e"+nodeId, redis);
+										}
+										else{
+											System.out.println("Insufficient data");
+										}
+										
+						}
+	            
+	        }
+							}
+						System.out.println("W00T, we loaded the edges");
+						}	
+				
+			}
+		}
+		
+		
+	}
+}
+
+            
+//.put(parts[0], parts[1]);
+		
+		
+  //      valSetOne.add("");
+    //    valSetOne.add("");
+        
+      
+
+	
+
--- src/main/java/evaluating/mm/redis/RedisClient.java
+++ src/main/java/evaluating/mm/redis/RedisClient.java
@@ -12,7 +12,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Protocol;
 import com.redislabs.modules.rejson.JReJSON;
 
-import evaluating.mm.redis.utils.Commands;
+//import evaluating.mm.redis.utils.Commands;
 
 public class RedisClient {
 	
@@ -22,59 +22,73 @@ public class RedisClient {
 	
 	public static void init () {
 		redis = new HashMap<>();
-		jedis = new Jedis("127.0.0.1", Protocol.DEFAULT_PORT);
+		jedis = new Jedis("127.0.0.1", Protocol.DEFAULT_PORT, 60000);
 		jedis.connect();
 	}
 	
 	public static void read () {
-		Set<String> keys = jedis.keys("*");
+		Set<String> keys = jedis.keys("v*");
 		Iterator<String> it = keys.iterator();
+		//Map<String, String> values = new HashMap<>();
 		while (it.hasNext()) {
 			String key= it.next();
+			
 			if (key.startsWith("v")||key.startsWith("e")){
-				Map<String, String> values = jedis.hgetAll(key);
-				redis.put(key, values);
+				redis.put(key, jedis.hgetAll(key));
 			}
 		}
-		
+	}
+	
+	public static void read (int i) {
+		redis.clear();
+		Set<String> keys = jedis.keys("e"+i+"*");
+		Iterator<String> it = keys.iterator();
+		//Map<String, String> values = new HashMap<>();
+		while (it.hasNext()) {
+			String key= it.next();
+			
+			if (key.startsWith("v")||key.startsWith("e")){
+				redis.put(key, jedis.hgetAll(key));
+			}
+		}
 	}
 	
 	//This converts all keys starting with v to vertexes and all starting with e to edges, in a graph with the name is passed as a parameter.
 	//So far this works for a single type of node, passed as parameter
 	//It assumes that the edge has a label (which defines the relationship type) and a src and tgt that have the nodeType values.
-	public static void convertToGraph(String graphName, String nodeType) {
+	public static void convertToGraph(String graphName, String nodeType, Boolean vertexes) {
 		String script = "return {redis.call('GRAPH.QUERY', KEYS[1], ARGV[1])}";
 		List<String> keys = new ArrayList<String>();
 	    keys.add(graphName);
+	    if (vertexes){
 		redis.entrySet().stream().filter(e->e.getKey().startsWith("v")).forEach(v->{
 			List<String> args = new ArrayList<String>();
-			String query= "'CREATE (";
-			query+=v.getValue().get(nodeType)+":"+nodeType+" ";
-			query+="{ ";
-			query+=v.getValue().entrySet().stream().filter(entry->entry.getKey()!=nodeType).map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			String query= "CREATE(";
+			query+=v.getKey()+":"+nodeType+"";
+			query+=" {";
+			query+=v.getValue().entrySet().stream().filter(entry->(!entry.getKey().equals(nodeType) && !entry.getKey().equals("desc"))).map(entry->entry.getKey()+":\""+entry.getValue().replaceAll("\"", "").replaceAll("\'",  "")+"\"").collect(Collectors.joining( ", " ) );
 			query+="})";
 			args.add(query);
+//			System.out.println(query);
 			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
-		    /*response.forEach(resp->{
-		    	System.out.println(resp.toString());
-		    });*/
+		   // response.forEach(resp->{
+		   // 	System.out.println(resp.toString());
+		    //});
 			
 		});
-		redis.entrySet().stream().filter(e->e.getKey().startsWith("e")).forEach(v->{
+	    }
+	    else{redis.entrySet().stream().filter(e->e.getKey().startsWith("e")).forEach(v->{
 			List<String> args = new ArrayList<String>();
 			String query= "'CREATE (";
 			query+=v.getValue().get("src");
 			query+=")-["+v.getValue().get("label")+" {";
-			query+=v.getValue().entrySet().stream().filter(entry->(entry.getKey()!="label")&&entry.getKey()!="src" && entry.getKey()!="tgt").map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
+			query+=v.getValue().entrySet().stream().filter(entry->(entry.getKey()!="label")&&entry.getKey()!="src" && entry.getKey()!="tgt").map(entry->entry.getKey()+":\""+entry.getValue()+"\"").collect(Collectors.joining( ", " ) );
 			query+="}]->";
 			query+="("+v.getValue().get("tgt");
 			query+=")";
 			args.add(query);
 			List<ArrayList<String>> response = (List<ArrayList<String>>) jedis.eval(script, keys, args);
-		    /*response.forEach(resp->{
-		    	System.out.println(resp.toString());
-		    });*/
-		});
+		});}
 	}
 	
 	
@@ -87,8 +101,8 @@ public class RedisClient {
 	    keys.add(graphName);
 		redis.entrySet().stream().filter(e->e.getKey().startsWith("v")).forEach(v->{
 			List<String> args = new ArrayList<String>();
-			String query= "'CREATE (";
-			query+=v.getValue().get(nodeType)+":"+nodeType+" ";
+			String query= "'CREATE(";
+			query+=v.getValue().get(nodeType)+":"+nodeType+"";
 			query+="{ ";
 			query+=v.getValue().entrySet().stream().filter(entry->entry.getKey()!=nodeType).map(entry->entry.getKey()+": "+entry.getValue()).collect(Collectors.joining( ", " ) );
 			query+="})";
@@ -135,13 +149,46 @@ public class RedisClient {
 	
 	public static void main(String[] args) {
 		long startTime, endTime=0;
+		int totalEdges= 0;
+		Boolean vertexes = Boolean.FALSE;
+		Boolean storedProcedure = Boolean.FALSE;
+		Boolean document = Boolean.TRUE;
 		init();
 		startTime=System.nanoTime();
-		read();
-		//convertToDocument();
-		convertToGraph("pokec", "profile");
+		if (document){
+			read();
+			convertToDocument();
+			for (int i=100; i<110; i++){
+				redis.clear();
+				read(i);
+				convertToDocument();				
+			}
+		}
+		else if (vertexes){
+			if (!storedProcedure){
+				read();
+				convertToGraph("pokec", "profile", vertexes);
+			}
+			else{
+				
+			}
+		}
+		else{
+			if (!storedProcedure){
+			for (int i=100; i<110; i++){
+				read(i);
+				//convertToDocument();
+				//convertToGraph("pokec", "profile", vertexes);
+				totalEdges+=redis.size();
+			}
+			}
+			else{
+				
+			}
+		}
 		endTime=System.nanoTime()-startTime;
 		System.out.println("Total duration = "+endTime);
+		System.out.println("Total edges"+totalEdges);
 		cleanup();
 	}
 
